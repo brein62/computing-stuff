66414c03591afd89c4df09115efe18987f91d883
Brendan Koh <brenstack62@gmail.com>
2019-07-03 22:35:22 +0800

    added code and evidence for PC8 - hash table

--- 2019 Programming Exercise/PC8 Hashtable Chaining - Hash Table/pc8.py
+++ 2019 Programming Exercise/PC8 Hashtable Chaining - Hash Table/pc8.py
@@ -16,7 +16,7 @@ class BookRec:
     def SetPointer(self, Pointer):
         self.__Pointer = Pointer
 
-class LinkedList:
+class LinkedList:                   # dynamic linked list
     def __init__(self):
         self.__Start = None         # null
     
@@ -25,10 +25,19 @@ class LinkedList:
 
     def DisplayLinkedList(self):
         curr = self.__Start         # current node
-        while curr != None:
-            print("{0} -> ".format(curr.GetBookID()))
-            curr = curr.GetPointer()
-
+        if curr == None:
+            #empty linked list
+            print("Empty linked list")
+        else:
+            print()
+            print("-" * 52)
+            print("| {0:^15} | {1:^30} |".format("BookID", "Title"))
+            print("-" * 52)
+            while curr != None:
+                print("| {0:^15} | {1:^30} |".format(curr.GetBookID(), curr.GetTitle()))
+                curr = curr.GetPointer()
+            print("-" * 52)
+        
     def AddNode(self, BookID, Title):
         NewNode = BookRec(BookID, Title, None)
         if self.IsEmpty():
@@ -44,9 +53,9 @@ class LinkedList:
         while curr != None and curr.GetBookID() != BookID:
             curr = curr.GetPointer()
         if curr == None:
-            return False
+            return False            # is within linked list
         else:
-            return True
+            return True             # is not in linked list
         # return (curr == None)
 
     def DeleteNode(self, BookID):
@@ -57,9 +66,58 @@ class LinkedList:
                 prev = curr
                 curr = curr.GetPointer()
             prev.SetPointer(curr.GetPointer())
-
-    
-    
         
+class HashTable:
+    def __init__(self, Size = 17):       # Initialise()
+        self.__Size = Size
+        self.__Slots = [None] + [LinkedList() for i in range(Size)]
+        # array of empty linked lists.
+        # self.__Slots[0] = None as hash table is 1-based.
 
-    
+    def Hash(self, BookID):
+        ASCIISum = 0
+        for character in BookID:
+            ASCIISum += ord(character)          # add ASCII value to ASCII sum
+        Address = (ASCIISum % self.__Size) + 1  # address = remainder + 1
+        return Address
+
+    def Display(self):
+        for i in range(self.__Size):
+            Address = i + 1
+            CurrentLinkedList = self.__Slots[Address]
+            print("[{0}] in hash table: ".format(Address), end='')
+            CurrentLinkedList.DisplayLinkedList()
+            print()
+
+    def Put(self, BookID, Title):
+        Address = self.Hash(BookID)
+        self.__Slots[Address].AddNode(BookID, Title)
+
+    def Remove(self, BookID):
+        Address = self.Hash(BookID)
+        CurrentLinkedList = self.__Slots[Address]
+        if CurrentLinkedList.SearchNode(BookID):
+            CurrentLinkedList.DeleteNode(BookID)
+            print("{0} has been removed from the hash table.".format(BookID))
+        else:
+            print("{0} cannot be removed as it is not found in the hash table.".format(BookID))
+            
+    def Search(self, BookID):
+        Address = self.Hash(BookID)
+        CurrentLinkedList = self.__Slots[Address]
+        if CurrentLinkedList.SearchNode(BookID):
+            return True
+        else:
+            return False
+        # alternatively: return CurrentLinkedList.SearchNode(BookID)
+
+Books = HashTable(17)
+Books.Put("CS733", "Basic algorithms")
+Books.Put("AB944", "Master Computing")
+Books.Put("KS293", "Data structures")
+Books.Put("BK232", "Programming exercises")
+Books.Put("PK199", "Testing Python")
+Books.Display()
+Books.Remove("AB944")
+Books.Display()
+print(Books.Search("PK199"))
